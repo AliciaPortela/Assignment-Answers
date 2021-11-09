0c50adb15bd6fef14ac2651c49d1f7e6120a0e39
AliciaPortela <alicia.portela@alumnos.UPM.es>
2021-11-09 10:47:40 -0500

    FINAL

--- Assignment 2/Assignment_2.rb
+++ Assignment 2/Assignment_2.rb
@@ -1,77 +1,129 @@
+######## this scritps does the following ########
+######## 1.gets the AGIs from the ArabidopsisSubNetwork_GeneList.txt file ########
+######## 2.searches for interactions between genes from that file ########
+######## 3.creates networks (instances) in the class InteractionNetwork with network, KEGG and GO annotations ########
+######## 4.writes a report with all the information about the performed analysis ########
+
+
+# in order to make a request to a server: PSICQUIC/TOGO
 require 'rest-client'
+# in order to handle '.json' format
 require 'json'
+# required to get the definition of the InteractionNetwork class
 require './InteractionNetwork.rb'
 
-
+##### This script is run in the command line as follows #####
+##### $ruby Assignment_2.rb ArabidopsisSubNetwork_GeneList.txt report.txt #####
+# using ARGV[]
+# passing the command line arguments to variables 
 file = "./"+ARGV[0]+""
 report = "./"+ARGV[1]+""
 
+##### getting the AGIs #####
 InteractionNetwork.getting_agi(file)
+##### searching for interactions #####
 InteractionNetwork.search_protein_int()
+##### creating networks (instances) #####
 InteractionNetwork.create_networks()
 
 
 ##### WRITING A REPORT OF THE RESULTS OBTAINED #####
 # creating a new .txt file, writing it line by line
 File.open(report, 'w') do |line|
-  line.puts "---------Analysis of direct and indirect interactions between a set of co-expressed genes----------"
+  line.puts "\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s---------ANALYSIS OF INTERACTIONS BETWEEN A SET OF PREDICTIVELY CO-EXPRESSED GENES----------"
+  line.puts "\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s\s---------------------PERFORMED BY: Alicia Portela Estévez----------------------"
   line.puts "\n"
-  line.puts "For the #{File.open(file){|f| f.read.count("\n")}} co-expressed genes, direct and indirect interactions between them have been analysed through PSICQUIC REST API"
+  line.puts "INTRODUCTION"
+  line.puts "A recent paper (DOI: 10.1371/journal.pone.0108567) executes a meta-analysis of a few thousand published co-expressed gene sets from Arabidopsis thaliana. They break these co-expression sets into ~20 sub-networks of <200 genes each, that they find consistently co-expressed with one another. One of this sub-networks has #{File.open('ArabidopsisSubNetwork_GeneList.txt'){|f| f.read.count("\n")}} predictively co-expressed genes"
   line.puts "\n"
-  line.puts "SCHEMATIC REPRESENTATION OF INTERACTIONS:" 
-  line.puts "-Direct interactions: A-B, in which A and B are genes that belong to the co-expressed set of genes"
-  line.puts "-Indirect interactions: A-B-C, in which A and C are genes that belong to the co-expressed set of genes, but not B, which is the intermediary"
+  line.puts "OBJECTIVE"
+  line.puts "The objective of this further analysis is testing if there is already information that links those #{File.open(file){|f| f.read.count("\n")}} genes into known regulatory networks, either in a direct or indirect way, and annotate members of each network with any possible KEGG Pathway or GO Terms."
   line.puts "\n"
-  line.puts "CRITERIA FOR SEARCHING FOR INTERACTIONS:"
-  line.puts "1. Arabidopsis thaliana species, filtered by a regex"
-  line.puts "2. MIscore > 0.485, as mentioned in 10.1093/database/bau131"
+  line.puts "MATERIAL & METHODS"
+  line.puts "Materials used in this analysis are:"
+  line.puts "1. The sub-network with #{File.open(file){|f| f.read.count("\n")}} predicted co-expressed genes."
+  line.puts "2. BAR database from University of Toronto, in order to search for interactions, either direct or indirect, between the #{File.open(file){|f| f.read.count("\n")}} co-expressed genes, through PSICQUIC REST API."
+  line.puts "3. UniProt database, in order to search for KEGG and GO annotations, through TOGO REST API."
   line.puts "\n"
-  line.puts "RESULTS:"
+  line.puts "Methods followed in this analysis are:"
+  line.puts "1. Type of interactions"
+  line.puts "In this analysis, 2 kind of interactions have been analysed:"
+  line.puts "-Direct interactions: A-B, in which A and B are genes that belong to the co-expressed sub-network."
+  line.puts "-Indirect interactions: A-B-C, in which A and C are genes that belong to the co-expressed sub-network, but not B, which is the intermediary."
+  line.puts "2. Criteria for searching for interactions"
+  line.puts "In this analysis, 2 criteria have been followed:"
+  line.puts "-Interactors must belong to Arabidopsis thaliana species. It has been achieved by filtering by a regex, corresponding to the AGI locus code."
+  line.puts "-MIscore > 0.485, as mentioned in the article with DOI: 10.1093/database/bau131."
+  line.puts "\n"
+  line.puts "RESULTS"
   line.puts "In total, #{InteractionNetwork.get_number_networks()} networks have been found"
   line.puts "Below, it is shown one by one the networks, with the following information:" 
   line.puts "1. Number of network"
-  line.puts "2. Type (direct/indirect) of interaction"
+  line.puts "2. Type of interaction: direct/indirect"
   line.puts "3. Members of the network"
   line.puts "4. KEGG annotations: KEGG ID and KEGG pathway"
   line.puts "5. GO annotations: GO ID and GO terms"
   InteractionNetwork.get_networks.each do |network| # for each interaction...
     line.puts "\n"
     line.puts "----------------------------------------------------------------------------------------------------------"
+    # write type and members
     line.puts "Network number #{network.network_num}"
+    # if network contains only 2 members is a direct interaction (A-B)
     if network.members.count == 2
       line.puts "Direct interaction"
       line.puts "Members: #{network.members[0]} and #{network.members[1]}"
+    # if network contains 3 members is an indirect interaction (A-B-C)
     elsif network.members.count == 3
       line.puts "Indirect interaction"
       line.puts "Members: #{network.members[0]} and #{network.members[2]}, with #{network.members[1]} as intermediary"
     end
-    # kegg annotations
+    # write kegg annotations
     line.puts "\n"
     line.puts "KEGG annotations:"
-    # only if there is information about the KEGG in the hash
+    # only if the KEGG hash is not empty
     if !network.kegg_pathway.empty?
-      # if there is only one key-values pair...
-      if network.kegg_pathway.count == 1
-        line.puts "The #{network.kegg_pathway.keys[0]} member has KEGG ID: #{network.kegg_pathway.values[0][0]} and KEGG pathway: #{network.kegg_pathway.values[0][1]}"
-      # if there is more than one key-values pair...
-      else 
+        # iterating over the hash (key-value)
         network.kegg_pathway.each do |key,values|
-          line.puts "The #{key} member has KEGG ID: #{values[0]} and KEGG pathway: #{values[1]}"
-        end 
-      end 
+          # values are always array datatype
+          # if values are not empty
+          if values.is_a?(Array) && !values.empty?
+            # if array has only one element...
+            if values.count == 1 
+              line.puts "The #{key} member has KEGG ID: #{values[0][0]} and KEGG pathway: #{values[0][1]}"
+            # if array has more than one element...
+            elsif values.count > 1
+              line.puts "The #{key} member has the following KEGG IDs/pathways:"
+              # iterating over the elements 
+              values.each do |value|
+                line.puts "\s#{value[0]},#{value[1]}\s"
+              end
+            end
+          # if array values is empty
+          else
+            line.puts "The #{key} member has not got any KEGG ID/pathway"
+          end 
+        end
+    # if KEGG hash is empty
     else 
       line.puts "There are not any KEGG ID/pathway for any member of this network"
     end
-    # go annotations
+    # write go annotations
+    # same code as keggs 
     line.puts "\n"
     line.puts "GO annotations:"
-    # only if there is information about the GO in the hash
     if !network.go_terms.empty?
-      if network.go_terms.count == 1
-        line.puts "The #{network.go_terms.keys[0]} member has GO ID: #{network.go_terms.values[0][0]} and GO terms: #{network.go_terms.values[0][1]}"
-      else 
-        network.go_terms.each do |key,values|
-          line.puts "The #{key} member has GO ID: #{values[0]} and GO terms: #{values[1]}"
+      network.go_terms.each do |key,values|
+        if values.is_a?(Array) && !values.empty?
+          if values.count == 1
+            line.puts "The #{key} member has GO ID: #{values[0][0]} and GO terms: #{values[0][1]}"
+          elsif values.count > 1
+            line.puts "The #{key} member has the following GO ID/terms:"
+            values.each do |value|
+              line.puts "\s#{value[0]},#{value[1]}\s"
+            end
+          end
+        else
+          line.puts "The #{key} member has not got any GO ID/term"
         end
       end
     else 
@@ -79,4 +131,9 @@ File.open(report, 'w') do |line|
     end 
     line.puts "----------------------------------------------------------------------------------------------------------"
   end
+  line.puts "\n"
+  line.puts "DISCUSSION"
+  line.puts "Given a sub-network of predictively co-expressed #{File.open(file){|f| f.read.count("\n")}} number of genes by the article DOI: 10.1371/journal.pone.0108567, the goal of this further analysis has been checking if there is already available information in databases about direct or indirect interactions between those genes, and thus, creating sub-networks of interaction. Also, each network has been annotated with possible KEGG pathways and GO terms of its members."
+  line.puts "This analysis has been able to find #{InteractionNetwork.get_number_networks()} networks, most of them as a result of indirect interactions between members of the sub-network list, with only one intermediary. Thus, maximum interaction depth developed in this analysis is 2. That is to say, direct interactions include 2 members of the sub-network list (A-B), whereas indirect interactions take into account 2 members of the sub-network list (A-C), which interact through only one intermediary (B)." 
+  line.puts "Although this analysis has performed well, further studies may do deeper interaction analyses in the future, with more intermediaries, that will lead to create greater networks."
 end
--- Assignment 2/InteractionNetwork.rb
+++ Assignment 2/InteractionNetwork.rb
@@ -1,5 +1,5 @@
-###### DEFINING InteractionNetwork class ######
-# this class contains all the interaction networks of'ArabidopsisSubNetwork_GeneList.txt' genes, as instances
+###### DEFINING InteractionNetwork CLASS ######
+# this class will contain all the interaction networks of'ArabidopsisSubNetwork_GeneList.txt' genes, as instances
 # each interaction network (instance) will have members, KEGG Pathways and GO Terms
 
 class InteractionNetwork
@@ -15,27 +15,31 @@ class InteractionNetwork
   # 1. assigning a number to each network
   attr_accessor :network_num
   # 2. members of the interacting network
-  # it will be an array containing all the AGI codes of the members of the interaction network
+  # it will be an array containing all the AGIs codes of the members of the interaction network
   attr_accessor :members
   # 3. KEGG pathways
+  # it will be a hash to store all possible KEGG ID/pathways that may have each member of the network
   attr_accessor :kegg_pathway
   # 4. GO Terms
+  # it will be a hash to store all possible GO ID/terms that may have each member of the network
   attr_accessor :go_terms
   
   ## initializing instance variables ##
   def initialize(params = {})
     # each time an instance is created with .new() method...
-    # 1. assigning a number to the network
+    # 1. assigning a number to the network to identify them
     @network_num = params.fetch(:network_num, 0)
     # 2. an array containing all the AGIs of members of the interaction network is created
     @members = params.fetch(:members, 'NA')
-    # 3. anotation of KEGG ID and pathway name of all memebers in the interaction network
+    # 3. anotation of KEGG ID and pathway name of all memebers in the interaction network --> hash
+    # it takes members as argument
     @kegg_pathway = annotate_kegg(members = @members)
-    # 4. anotation of GO ID and GO terms of all members of the interaction network
+    # 4. anotation of GO ID and GO terms of all members of the interaction network --> hash
+    # it also takes members as argument
     @go_terms = annotate_go(members = @members)
     # 5. the total number of instances increases by 1
     @@number_networks +=1
-    # 3. all instances stored in the networks array
+    # 6. the instance is stored in the instances array
     @@all_networks << self 
   
   end
@@ -44,13 +48,15 @@ class InteractionNetwork
   ## 1. creating an array with all AGIs in lowercase codes from the file ('ArabidopsisSubNetwork_GeneList.txt')
   # lowercase is needed to compare with the AGIs from the web
   def self.getting_agi(file)
+    # for each line of the .text file...
     File.open(file).each do |gene|
+      # pass the content of the line to @@agis array class variable
       @@agis << gene.strip.downcase
     end
     return @@agis
   end
   
-  ## 2. attempts to contact the website and handles possible errors ##
+  ## 2. attempting to contact the webs and handling possible errors ##
   def self.fetch(url, headers = {accept: "*/*"}, user = "", pass="") 
   response = RestClient::Request.execute({
     method: :get,
@@ -74,9 +80,9 @@ class InteractionNetwork
     return response  # now we are returning 'False', and we will check that with an \"if\" statement in our main code
   end
   
-  ## 3. searches for protein-protein interaction in the Bar database ##
+  ## 3. searches for interactions in BAR database ##
   def self.search_protein_int() 
-    # creating an empty class variable hash to store the interactions
+    # creating an empty class variable hash to store interactions
     @@interactions = Hash.new
     # making a list equal to the @@agis class variable, where AGIs are stored
     agis = @@agis
@@ -89,18 +95,18 @@ class InteractionNetwork
         # res.body returns data, in which each interaction is separated by new line
         # creating an array in which each element is an interaction
         body = res.body.split("\n") 
-        # for each interaction for this AGI...
+        # for each interaction...
         body.each do |int| 
           # ...give me an array in which each element is a column 
           array = int.split("\t") 
           # indices 2 and 3 array (columns 3 and 4 of data) --> interactors
           ##### filtering by only Arabidopsis thaliana with regex #####
           # go to the next iteration if nil values
-          next if array[2].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/) == nil
-          next if array[3].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/) == nil 
+          next if array[2].match(/(A[Tt]\w[Gg]\d+)/) == nil
+          next if array[3].match(/(A[Tt]\w[Gg]\d+)/) == nil 
           # saving data in variables and converting to string
-          int_1 = array[2].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/)[0].to_s
-          int_2 = array[3].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/)[0].to_s
+          int_1 = array[2].match(/(A[Tt]\w[Gg]\d+)/)[0].to_s
+          int_2 = array[3].match(/(A[Tt]\w[Gg]\d+)/)[0].to_s
           # index 14 array (column 15 of data) --> MIscore
           # saving matched score with regex in variable and converting to float 
           miscore = array[14].match(/(0\.\d+)/)[0].to_f
@@ -130,10 +136,11 @@ class InteractionNetwork
               body2 = res.body.split("\n") 
               body2.each do |int2| 
                 array = int2.split("\t") 
-                next if array[2].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/) == nil
-                next if array[3].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/) == nil
-                int_12 = array[2].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/)[0].to_s
-                int_22 = array[3].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/)[0].to_s
+                next if array[2].match(/(A[Tt]\w[Gg]\d+)/) == nil
+                next if array[3].match(/(A[Tt]\w[Gg]\d+)/) == nil
+                int_12 = array[2].match(/(A[Tt]\w[Gg]\d+)/)[0].to_s
+                int_22 = array[3].match(/(A[Tt]\w[Gg]\d+)/)[0].to_s
+                ## adding the condition that the new interactor is in the AGIs list and is not the same at the first one used for searching
                 if int_12.downcase == int_2.downcase && int_22.downcase != int_2.downcase && agis.include?(int_22.downcase) && int_22.downcase != agi
                   # save the intermediary AGI that is not in the list and indirect interactor that is in the list with AGI used for search as key
                   @@interactions[agi.upcase] = [int_2.upcase,int_22.upcase]
@@ -154,10 +161,10 @@ class InteractionNetwork
               body3 = res.body.split("\n") 
               body3.each do |int3| 
                 array = int3.split("\t") 
-                next if array[2].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/) == nil
-                next if array[3].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/) == nil
-                int_123 = array[2].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/)[0].to_s
-                int_223 = array[3].match(/(A[Tt]\w[Gg]\d\d\d\d\d)/)[0].to_s
+                next if array[2].match(/(A[Tt]\w[Gg]\d+)/) == nil
+                next if array[3].match(/(A[Tt]\w[Gg]\d+)/) == nil
+                int_123 = array[2].match(/(A[Tt]\w[Gg]\d+)/)[0].to_s
+                int_223 = array[3].match(/(A[Tt]\w[Gg]\d+)/)[0].to_s
                 if int_123.downcase == int_1.downcase && int_223.downcase != int_1.downcase && agis.include?(int_223.downcase) && int_223.downcase != agi
                   @@interactions[agi.upcase] = [int_1.upcase,int_223.upcase]
                 elsif int_123.downcase != int_1.downcase && int_223.downcase == int_1.downcase && agis.include?(int_123.downcase) && int_123.downcase != agi
@@ -186,14 +193,14 @@ class InteractionNetwork
       members = Array.new
       # increase by 1 the number of the network
       number += 1
-      # store in members array members of indirect interactions
+      # store in array members of indirect interactions: values = array, key = string
       if value.is_a?(Array) && key.is_a?(String)
         value.each do |val|
           members << val
         end
         members << key
         InteractionNetwork.new(:network_num => number, :members => members)
-      # store in members array members of direct interactions
+      # store in array members of direct interactions: values = string, key = string
       elsif value.is_a?(String) && key.is_a?(String)
         members = [key,value]
         InteractionNetwork.new(:network_num => number, :members => members)
@@ -213,21 +220,24 @@ class InteractionNetwork
   
   
   ## DEFINING INSTANCE METHODS ##
-  ## 1. annotate KEGG ID and KEGG pathways for members of a network ##
+  ## 1. annotate KEGG ID and KEGG pathways for members of a sinlge network ##
   def annotate_kegg(members)
     # creating an empty array to store KEGG annotations
     kegg_annotations = Hash.new
     # for each member of the network...
     members.each do |member|
+      # creating an empty array to store info about a member (key)
+      kegg_annotations[member] = Array.new
       res = InteractionNetwork.fetch("http://togows.org/entry/genes/ath:#{member}/pathways.json")
       if res 
+        # parse a JSON document into a ruby data structure
         data = JSON.parse(res.body)
         # data[0] is a hash 
         # iterating for each entry of this hash
         # key and value
         data[0].each do |kegg_id,kegg_p|
-          # save key --> KEGG ID and value --> KEGG pathway in the array
-          kegg_annotations[member] = [kegg_id,kegg_p]
+          # saving each KEGG ID and KEGG pathway as a value (array) of the key
+          kegg_annotations[member] << [kegg_id,kegg_p]
         end 
       else 
         puts "the Web call failed - see STDERR for details...kegg"
@@ -237,21 +247,24 @@ class InteractionNetwork
   end
   ## 2. annotate GO ID and GO terms for members of a network ##
   def annotate_go(members)
-    # creating an empty array to store GO annotations
+    # same as KEGGs 
     go_annotations = Hash.new
-    # for each member of the network...
     members.each do |member|
+      go_annotations[member] = Array.new
       res = InteractionNetwork.fetch("http://togows.org/entry/ebi-uniprot/#{member}/dr.json")
       if res 
         data = JSON.parse(res.body)
+        # want to take the key "GO"
+        # and iterate over the values (lists)
         data[0]["GO"].each do |go|
           # go to the next iteration if the first entry does not match a GO ID
           next unless go[0].match(/GO:\d+/)
           # go to the next iteration if the second entry does not match a p = biological process, followed by string
           next unless go[1].match(/P:\w+/)
-          go_id = go[0]
+          # using regex to only take the number and the description as variables 
+          go_id = go[0].match(/(\d+)/)[1]
           go_term = go[1].match(/:(.+)/)[1]
-          go_annotations[member] = [go_id,go_term]
+          go_annotations[member] << [go_id,go_term]
         end
       else 
         puts "the Web call failed - see STDERR for details...go"
